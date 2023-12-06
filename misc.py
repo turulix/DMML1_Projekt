551a60e82af4889a0374c3a0ab72305b917df68b
turulix <tigga01@online.de>
2023-12-06 10:10:35 +0100

    Misc

---
+++ misc.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+def get_train_data(rawdata=None, preCustomers=False) -> (pd.DataFrame, pd.DataFrame,
+                                                         pd.DataFrame, pd.Series, pd.DataFrame, pd.Series):
+    """
+    :param rawdata: The data to use. If None, the data will be loaded from the csv files.
+    :param preCustomers: If True, the data will be prepared for the prediction of customers. If False, the data will be
+    prepared for the prediction of sales.
+    :return: The data, The Processed Data, the train and test data for the x values and the train and test data for the y values.
+    """
+    if rawdata is None:
+        stores = pd.read_csv("./data/dmml1_stores.csv")
+        rawdata = pd.read_csv("./data/dmml1_train.csv")
+        rawdata = rawdata.merge(stores, on="Store ID")
+        rawdata = rawdata.loc[rawdata["Open"] == 1]
+        rawdata["Date"] = pd.to_datetime(rawdata["Date"])
+
+    column_transformer = ColumnTransformer([
+        ("Drop Unused", "drop", [
+            "Store ID",
+            "Date",
+            "Open",
+            "PromoInterval"
+        ]),
+        ("One Hot Encode", OneHotEncoder(handle_unknown="ignore"), ["StateHoliday", "StoreType", "Assortment"]),
+        ("Scale", StandardScaler(), [
+            "CompetitionDistance",
+            "CompetitionOpenSinceMonth",
+            "CompetitionOpenSinceYear",
+            "Promo2SinceWeek",
+            "Promo2SinceYear",
+        ]),
+    ], remainder="passthrough")
+    rawdata.dropna(inplace=True)
+    data = pd.DataFrame(column_transformer.fit_transform(rawdata), columns=column_transformer.get_feature_names_out())
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        data.drop(columns=["remainder__Customers", "remainder__Sales"]),
+        data["remainder__Customers" if preCustomers else "remainder__Sales"],
+        test_size=0.2,
+        random_state=42
+    )
+    return rawdata, data.drop(columns=["remainder__Customers", "remainder__Sales"]), x_train, x_test, y_train, y_test
