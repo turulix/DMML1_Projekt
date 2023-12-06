05171212ebd2eaf936514f2ee8ee464415989bdf
turulix <tigga01@online.de>
2023-12-06 15:24:40 +0100

    Moving Files.

--- training/misc.py
+++ training/misc.py
@@ -4,16 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
-def get_train_data(rawdata=None, preCustomers=False) -> (pd.DataFrame, pd.Series):
+def get_train_data(rawdata=None) -> (pd.DataFrame, pd.Series):
     """
     :param rawdata: The data to use. If None, the data will be loaded from the csv files.
-    :param preCustomers: If True, the data will be prepared for the prediction of customers. If False, the data will be
-    prepared for the prediction of sales.
     :return: The data, The Processed Data, the train and test data for the x values and the train and test data for the y values.
     """
     if rawdata is None:
-        stores = pd.read_csv("./data/dmml1_stores.csv")
-        rawdata = pd.read_csv("./data/dmml1_train.csv")
+        stores = pd.read_csv("../data/dmml1_stores.csv")
+        rawdata = pd.read_csv("../data/dmml1_train.csv")
         rawdata = rawdata.merge(stores, on="Store ID")
         rawdata = rawdata.loc[rawdata["Open"] == 1]
         rawdata["Date"] = pd.to_datetime(rawdata["Date"])
@@ -27,6 +25,7 @@ def get_train_data(rawdata=None, preCustomers=False) -> (pd.DataFrame, pd.Series
         ]),
         ("One Hot Encode", OneHotEncoder(handle_unknown="ignore"), ["StateHoliday", "StoreType", "Assortment"]),
         ("Scale", StandardScaler(), [
+            "Customers",
             "CompetitionDistance",
             "CompetitionOpenSinceMonth",
             "CompetitionOpenSinceYear",
@@ -38,6 +37,7 @@ def get_train_data(rawdata=None, preCustomers=False) -> (pd.DataFrame, pd.Series
     data = pd.DataFrame(column_transformer.fit_transform(rawdata), columns=column_transformer.get_feature_names_out())
 
     return (
-        data.drop(columns=["remainder__Customers", "remainder__Sales"]),
-        data["remainder__Customers" if preCustomers else "remainder__Sales"]
+        #data.drop(columns=["remainder__Customers", "remainder__Sales"]),
+        data.drop(columns=["remainder__Sales"]),
+        data["remainder__Sales"]
     )
