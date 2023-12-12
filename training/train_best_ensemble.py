14bc333c088c585a75c4217cc5dd7e62e3467532
turulix <tigga01@online.de>
2023-12-12 11:26:26 +0100

    Do things better, faster, stronger

---
+++ training/train_best_ensemble.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+
+from training.misc import get_train_data
+
+def main():
+    features, target = get_train_data(use_train=True)
+
+    params = {
+        "val_size": 0.2,
+        "n_estimators": 100,
+        "max_depth": 5,
+        "max_features": 8,
+    }
+
+    features, x_val, target, y_val = train_test_split(features, target, test_size=params["val_size"], random_state=42)
+
+    gradient_model = GradientBoostingRegressor(
+        n_estimators=params["n_estimators"],
+        max_depth=params["max_depth"],
+        max_features=params["max_features"],
+    )
+
+    gradient_model.fit(features, target)
+
+    print(gradient_model.score(x_val, y_val))
+
+    x_val.reset_index(drop=True, inplace=True)
+    y_val.reset_index(drop=True, inplace=True)
+
+    predictions = gradient_model.predict(x_val)
+
+    with_predictions = pd.concat([x_val, y_val, pd.DataFrame(predictions, columns=["Prediction"])], axis=1)
+    with_predictions["Error"] = with_predictions["Prediction"] - with_predictions["Sales"]
+    # Set Penalty to 150 when Error > 3000
+    # with_predictions["Penalty"] = with_predictions["Error"].loc[with_predictions["Error"] < -4000, "Error"] = 100
+    # with_predictions["Penalty"] = with_predictions.loc[with_predictions["Error"] > 3000, "Error"] = 150
+    # with_predictions["Penalty"] = with_predictions.loc[with_predictions["Error"] > 6000, "Error"] = 250
+    # with_predictions["Penalty"] = with_predictions["Penalty"].fillna(0)
+
+    with_predictions["Penalty"] = np.where(with_predictions["Error"] < -4000, 100, 0)
+    with_predictions["Penalty"] = np.where(with_predictions["Error"] > 3000, 150, with_predictions["Penalty"])
+    with_predictions["Penalty"] = np.where(with_predictions["Error"] > 6000, 250, with_predictions["Penalty"])
+
+    with_predictions = with_predictions[with_predictions["remainder__Open"] == 1]
+
+    print(with_predictions.head(10))
+
+if __name__ == "__main__":
+    main()
