419206f1f86315b85f3bf116d14e51e29d24dcef
turulix <tigga01@online.de>
2023-12-12 11:40:02 +0100

    Using better Parameters

--- training/agent_ensemble.py
+++ training/agent_ensemble.py
@@ -10,17 +10,18 @@ project = "DMML1_Projekt_Tim"
 # Settings for the WandB Sweep, we are using a grid search here.
 sweep_configuration = {
     "method": "grid",
-    "name": f"{model_type} - Predicting Sales with PromoInterval",
+    "name": f"{model_type} - Predicting Sales with PromoInterval & learning_rate",
     "metric": {"goal": "maximize", "name": "mean_val_score"},
     "parameters": {
         #  "model": {"values": ["random_forest", "gradient_boosting"]},
-        "n_estimators": {"values": [10, 50, 100, 200, 400]},
+        "n_estimators": {"values": [100, 200, 400]},
         "max_depth": {"values": [3, 5, 10]},
-        "max_features": {"values": [1, 2, 4, 8, 12, 16, None, "sqrt", "log2"]},
+        "max_features": {"values": [16, 20, 28]},
+        "learning_rate": {"values": [0.1, 0.01, 0.001, 0.0001]},
 
         # The following parameters are not used by the model, but are used by the training script.
-        "val_size": {"values": [0.2, 0.3]},
-        "n_folds": {"values": [2, 5]},
+        "val_size": {"values": [0.2]},
+        "n_folds": {"values": [5]},
     },
 }
 
@@ -51,6 +52,7 @@ def main():
 
     model = GradientBoostingRegressor(
         n_estimators=run.config.n_estimators,
+        learning_rate=run.config.learning_rate,
         max_depth=run.config.max_depth,
         max_features=run.config.max_features,
     )
